abd3c507679f287225a4580a374f7319d467db24
Skip McBride <skip@angerbunny.com>
2020-02-24 18:47:06 -0800

    added authenticator

--- src/pages/Authenticate/Authenticate.js
+++ src/pages/Authenticate/Authenticate.js
@@ -1,6 +1,23 @@
-
+import React, { useState, FormEvent, useEffect } from 'react';
+import * as service from '../../services/apiRequestors';
+import AuthButton from '../../components/AuthButton/AuthButton';
+import 'Authenticate.scss';
 
 const Authenticate = (props) => {
+  const [inputIp, setInnputIp] = useState('');
+  const [inputDevName, setInputDev] = useState('');
+  const [authenticateWaiter, setAuthenticateWaiter] = useState(false);
+  const [authenticateTimeout, setAuthenticateTimeout] = useState(null);
+
+  useEffect(() => {
+    startAuthenticating();
+  }, [authenticateWaiter]);
+
+  const onInputIpChange = (inputChange) => {
+    return (e) => {
+      inputChange(e.currentTarget.value);
+    };
+  };
 
   return props;
 }
