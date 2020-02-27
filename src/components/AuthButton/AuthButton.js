8682e30936070ffaa8735c35ec744d723b00e4a0
Skip McBride <skip@angerbunny.com>
2020-02-26 19:28:10 -0800

    added authing button and authing goodies

--- src/components/AuthButton/AuthButton.js
+++ src/components/AuthButton/AuthButton.js
@@ -1,9 +1,20 @@
-
+import React from 'react';
+import './AuthButton.scss';
 
 const AuthButton = (props) => {
+  const buttonStyle = props.disabled ? 'authButton disabled' : 'authButton';
 
+  const handleClick = () => {
+    if (!props.disabled) {
+      props.onClick();
+    }
+  };
 
-  return true;
+  return (
+    <div className={buttonStyle} onClick={handleClick}>
+      {props.hint}
+    </div>
+  )
 }
 
 
